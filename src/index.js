185a65ec14b8b9f9d95f40fb43fb05ce1e18ac4c
codefug <robot9917@gmail.com>
2023-12-26 08:34:57 +0900

    add boiler plate of the project, webpack setting, src environment setting

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import './styles/contact.css';
+import './styles/home.css';
+import './styles/loading.css';
+import './styles/store.css';
+import './styles/streamer.css';
+import home from './tabs/home.js';
+import homefunction from './functions/home.js'
+import contact from './tabs/contact.js';
+import contactfunction from './functions/contact.js'
+import loading from './tabs/loading.js';
+import loadingfunction from './functions/loading.js'
+import store from './tabs/store.js';
+import storefunction from './functions/store.js'
+import streamer from './tabs/streamer.js';
+import streamerfunction from './functions/streamer.js'
+
---
+++ webpack.common.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename:'index.html',
+            inject:'head',
+            scriptLoading:'defer'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/i,
+                use:['style-loader','css-loader'],
+            },
+        ],
+    },
+};
